1ba6aa120fe030795e9e3fac6aa6ac49525161a2
SinWasin <59160898@go.buu.ac.th>
2020-10-14 12:39:01 +0700

    add chart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as Chart from 'chart.js';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  canvas: any;
+  ctx: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+    const myChart = new Chart(this.ctx, {
+      type: 'bar',
+      data: {
+        labels: ["มหาวิทยาลัยบูรพา", "ชายหาดบางแสน", "แหลมแท่น บางแสน", "หาดวอนนภา"],
+        datasets: [{
+          label: "ขวดแก้ว",
+          backgroundColor: "#5B9BD5",
+          data: [10, 20, 30, 80]
+        },
+        {
+          label: "ขวดพลาสติก",
+          backgroundColor: '#ED7D31',
+          data: [70, 40, 10, 100]
+        },
+        {
+          label: "กระป๋องโลหะ",
+          backgroundColor: '#A5A5A5',
+          data: [80, 50, 90, 27]
+        }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              min: 0,
+              max: 100,
+              callback: function (value) {
+                return value + "%"
+              }
+            }
+          }]
+        },
+        responsive: false
+      }
+    });
   }
 
 }
+
--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as Chart from 'chart.js';
+
+
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
+  canvas: any;
+  ctx: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+    const myChart = new Chart(this.ctx, {
+      type: 'bar',
+      data: {
+        labels: ["ก.ค.-63", "ส.ค.-63", "ก.ย.-63", "ต.ค.-63"],
+        datasets: [{
+          label: "ขวดแก้ว",
+          backgroundColor: "#5B9BD5",
+          data: [10, 20, 30, 80]
+        },
+        {
+          label: "ขวดพลาสติก",
+          backgroundColor: '#ED7D31',
+          data: [70, 40, 10, 100]
+        },
+        {
+          label: "กระป๋องโลหะ",
+          backgroundColor: '#A5A5A5',
+          data: [80, 50, 90,27]
+        }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              min: 0,
+              max: 100,
+            }
+          }]
+        },
+        responsive: false
+      }
+    });
   }
-
 }
+
